7b069a2908416a3a34b121870c93899fc5c34595
Chanikya Uppalapati <chanikya.uppalapati@gmail.com>
2019-07-23 16:21:52 +0530

    Assets

--- Brp/assets/Scripts/Register.js
+++ Brp/assets/Scripts/Register.js
@@ -1,11 +1,11 @@
 ﻿/*------------------------------------------------------------------
 [SignIn Javascript]
-Project:	SVCE
+Project:	BIM Insights
 Author: Connex Info Systems
 Version:	1.0
 Designed by: Chanikya Uppalapati
 Author URI: http://connexinfo.com
--------------------------------------------------------------------*/
+------------------------------------------------------------------*/
 
 $(document).ready(function () {
     //Start SignIn
