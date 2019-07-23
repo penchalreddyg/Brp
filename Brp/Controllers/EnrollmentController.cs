5ea078fa866510e7605d7c3dfdcc3d3feadc4385
Penchal Reddy Geethala <penchal@connexinfo.com>
2019-07-23 19:45:21 +0530

    wired up grid

--- Brp/Controllers/EnrollmentController.cs
+++ Brp/Controllers/EnrollmentController.cs
@@ -33,21 +33,21 @@ namespace Brp.Controllers
             {
                 var response = new ConsumerResponse();
                 var objReq = new ConsumerRequest();
-                objReq.RecordsReturned = 5;
+                objReq.RecordsReturned = 50;
                 objReq.GrantID = 5;
                 objReq.RequestBy = "BIMInsights-QA";
                 objReq.ServiceId = 1400;
                 objReq.PassPort = new Passport { PassKey = "BIMInsightsQA", PassPhrase = "5E758B95-FC80-414A-AA20-493DB315B90C" };
                 var url = "api/EnrollmentServices/ConsumerStates";
                 response = new ServiceClient().RunGetAsync<ConsumerRequest, ConsumerResponse>(objReq, url);
-                return View("Consumer", response);
+                return View("ConsumerSummary", response);
             }
             catch (Exception ex)
             {
 
                 throw ex;
             }
-          
+
         }
 
     }
