39a2ef92a7ec6c39c1c9408bd78e182268c8ad05
Chanikya Uppalapati <chanikya.uppalapati@gmail.com>
2020-11-25 20:10:58 +0530

    Response Codes

--- Brp/Controllers/EnrollmentController.cs
+++ Brp/Controllers/EnrollmentController.cs
@@ -124,6 +124,27 @@ namespace Brp.Controllers
                 throw ex;
             }
         }
+        public ActionResult ResponseCodes()
+        {
+            try
+            {
+                var response = new ConsumerResponse();
+                var objReq = new ConsumerRequest();
+                objReq.RecordsReturned = 50;
+                objReq.GrantID = 5;
+                objReq.RequestBy = "BIMInsights-QA";
+                objReq.ServiceId = 1400;
+                objReq.PassPort = new Passport { PassKey = "BIMInsightsQA", PassPhrase = "5E758B95-FC80-414A-AA20-493DB315B90C" };
+                var url = "api/EnrollmentServices/ConsumerStates";
+                response = new ServiceClient().RunPut<ConsumerRequest, ConsumerResponse>(objReq, url);
+                return View("ResponseCodes", response);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
 
     }
 }
